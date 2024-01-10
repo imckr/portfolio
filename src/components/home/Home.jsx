4da0484b4f279adca28ff7a62b1ead120c82d764
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-01-10 22:45:30 +0530

    background slogan added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 // import Welcome from '../Welcome/Welcome';
 import { gsap } from "gsap";
 import SplitType from "split-type";
+import Background from "../../container/Background";
 
 const Home = () => {
   const text = new SplitType(".text", { types: "chars" });
@@ -22,12 +23,13 @@ const Home = () => {
       stagger: 0.1,
       duration: 1,
       ease: "back.inOut",
-      repeat: -1
+      repeat: -1,
     }
   );
 
   return (
     <div className="home-page">
+      <Background/>
       <div className="home-name">
         <h1 className="text">Hii,</h1><br />
         <h1 className="text">im__ckr</h1>
---
+++ src/container/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Background.css'
+
+const Background = () => {
+  return (
+    <div className='port__background-slogan'>
+      {/* <div className="port__bg-text"> */}
+        <h1 className="slogan-line1">INSPIRE</h1>
+        <h1 className="slogan-line2">HOPE &</h1>
+      {/* </div> */}
+    </div>
+  )
+}
+
+export default Background
