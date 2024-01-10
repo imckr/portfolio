02c50287ddd5c05564c962357206c47dfe88ef6e
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-01-10 20:53:42 +0530

    name added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from 'react'
 import './App.css'
 import {Home, Navbar, Welcome} from './components'
-import { gsap } from "gsap";
-import SplitType from "split-type";
+// import { gsap } from "gsap";
+// import SplitType from "split-type";
 
 
 const App = () => {
 
-  const text = new SplitType(".text", { types: "chars" });
-  // alert(text)
-  const char = text.chars;
-  // alert(char.toString)
-
-  gsap.fromTo(
-    char,
-    {
-      y: 100,
-      opacity: 0,
-    },
-    {
-      y: 0,
-      opacity: 1,
-      stagger: 0.1,
-      duration: 1,
-      ease: "power4.out",
-      repeat: 4,
-    }
-  );
-
   return (
     <div className="App">
-      {/* <div className='home-line'></div> */}
       <Welcome />
       <Home />
       <Navbar />
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,33 +2,36 @@ import React from "react";
 import "./Home.css";
 // import Welcome from '../Welcome/Welcome';
 import { gsap } from "gsap";
-// import SplitType from "split-type";
+import SplitType from "split-type";
 
 const Home = () => {
-  // gsap.to(".text", { duration:})
-  // const text = new SplitType(".text", { types: "chars" });
-  // // alert(text)
-  // const char = text.chars;
-  // // alert(char.toString)
+  const text = new SplitType(".text", { types: "chars" });
+  // alert(text)
+  const char = text.chars;
+  // alert(char.toString)
 
-  // gsap.fromTo(
-  //   char, 
-  //   {
-  //     y : 100,
-  //     opacity : 0
-  //   },
-  //   {
-  //     y : 0,
-  //     opacity : 1,
-  //     stagger : 0.1,
-  //     duration : 1,
-  //     ease : 'power4.out',
-  //     repeat: 1,
-  //   });
+  gsap.fromTo(
+    char,
+    {
+      y: 100,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      stagger: 0.1,
+      duration: 1,
+      ease: "back.inOut",
+      repeat: -1
+    }
+  );
 
   return (
     <div className="home-page">
-      {/* <Welcome/> */}
+      <div className="home-name">
+        <h1 className="text">Hii,</h1><br />
+        <h1 className="text">im__ckr</h1>
+      </div>
     </div>
   );
 };
