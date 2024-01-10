03103d42104d080e1995db37b9f6f5c700e8d08b
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-01-10 20:23:39 +0530

    Half animation part completed

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,81 +1,85 @@
-import React from 'react'
-import './Welcome.css'
+import React from "react";
+import "./Welcome.css";
+import { gsap } from "gsap";
 
 const Welcome = () => {
+
+  let name = "011010";
+       
   return (
-    <div class="marquee-wrapper">
-      <div class="container">
-        <div class="marquee-block">
-          <div class="marquee-inner to-left">
-            <span>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-            </span>
-            <span>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-              <div class="marquee-item">
-                <p class="text-white">Welcome</p>
-              </div>
-            </span>
-          </div>
+    <div class="upper">
+      <div class="inner">
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+      </div>
+      <div class="inner">
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
+        </div>
+        <div class="number odd">
+          <p>{name}</p>
+        </div>
+        <div class="number">
+          <p>{name}</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Welcome
+export default Welcome;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import img1 from '../../assets/computer mouse.svg'
-import './Home.css'
-import Welcome from '../Welcome/Welcome';
+import React from "react";
+import "./Home.css";
+// import Welcome from '../Welcome/Welcome';
+import { gsap } from "gsap";
+// import SplitType from "split-type";
 
 const Home = () => {
+  // gsap.to(".text", { duration:})
+  // const text = new SplitType(".text", { types: "chars" });
+  // // alert(text)
+  // const char = text.chars;
+  // // alert(char.toString)
+
+  // gsap.fromTo(
+  //   char, 
+  //   {
+  //     y : 100,
+  //     opacity : 0
+  //   },
+  //   {
+  //     y : 0,
+  //     opacity : 1,
+  //     stagger : 0.1,
+  //     duration : 1,
+  //     ease : 'power4.out',
+  //     repeat: 1,
+  //   });
+
   return (
-    <div className='home-page'>
-      <Welcome/>
+    <div className="home-page">
+      {/* <Welcome/> */}
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
